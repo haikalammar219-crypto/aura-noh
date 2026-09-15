1c79f2d7a1d9c66a9b10aaf9e366040096383f1a
haikalammar219-crypto <haikalammar219@gmail.com>
2026-09-15 23:01:37 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 export default {
   async fetch(request, env) {
-    return await env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+    let response = await env.ASSETS.fetch(request);
+
+    // إذا كان الرابط الرئيسي أو الملف غير موجود، يعرض index.html
+    if (response.status === 404 || url.pathname === "/") {
+      return await env.ASSETS.fetch(new Request(new URL("/index.html", request.url), request));
+    }
+    return response;
   },
 };
